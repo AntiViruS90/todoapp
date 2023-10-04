ea4294812306ff4097b838fd9949ee10c58abf91
AntiViruS90 <131804906+AntiViruS90@users.noreply.github.com>
2023-10-04 18:58:54 +0300

    App Todo

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,11 @@
-from django.db import models
+from django.db import models as m
 
-# Create your models here.
+
+class Todo(m.Model):
+    title = m.CharField(max_length=200)
+    description = m.CharField(max_length=300, blank=True)
+    date = m.DateTimeField(auto_now_add=True)
+    done = m.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title

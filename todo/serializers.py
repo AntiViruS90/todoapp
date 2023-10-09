05e08ac9be6f14dfc24750a41628903e6c9f2fda
AntiViruS90 <131804906+AntiViruS90@users.noreply.github.com>
2023-10-09 12:17:30 +0300

    some changes in serializers.py

--- todo/serializers.py
+++ todo/serializers.py
@@ -2,7 +2,13 @@ from rest_framework import serializers
 from .models import Todo
 
 
-class TodoSerializer(serializers.Serializer):
+# class TodoSerializer(serializers.Serializer):
+#     class Meta:
+#         model = Todo
+#         fields = '__all__'
+
+
+class TodoSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Todo
-        fields = '__all__'
+        fields = ['title', 'description', 'date', 'done']
